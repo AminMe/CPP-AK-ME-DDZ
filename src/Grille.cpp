3e59d2945c399cd792f8178a4ecddd325a9fd31e
Amin Messaoudi <amin_messaoudi@yahoo.com>
2015-01-20 12:28:43 +0100

    init

---
+++ src/Grille.cpp
@@ -0,0 +1,18 @@
+/*
+ * Grille.cpp
+ *
+ *  Created on: 20 janv. 2015
+ *      Author: Amin
+ */
+
+#include "Grille.h"
+
+Grille::Grille() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Grille::~Grille() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ src/Grille.h
@@ -0,0 +1,17 @@
+/*
+ * Grille.h
+ *
+ *  Created on: 20 janv. 2015
+ *      Author: Amin
+ */
+
+#ifndef GRILLE_H_
+#define GRILLE_H_
+
+class Grille {
+public:
+	Grille();
+	virtual ~Grille();
+};
+
+#endif /* GRILLE_H_ */
