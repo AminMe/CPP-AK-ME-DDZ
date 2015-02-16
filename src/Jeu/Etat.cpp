9cd088bcc9a5dbc6c84af34cada029b658777b0e
Amin Messaoudi <amin_messaoudi@yahoo.com>
2015-02-16 17:44:00 +0100

    Merge effectué

--- src/Jeu/Etat.cpp
+++ src/Jeu/Etat.cpp
@@ -14,9 +14,9 @@ bool Etat::isFinished()
 {
 	Map &map = Map::Instance();
 
-	for (int i = 1; i<Map::LIGNE-1; i++)
+	for (int i = 1; i<LIGNE-1; i++)
 	{
-		for (int j = 1; i<Map::COLONNE-1; i++)
+		for (int j = 1; i<COLONNE-1; i++)
 		{
 			pair<int, int> index(i,j);
 			if(!map[index].isEstOccupe())
