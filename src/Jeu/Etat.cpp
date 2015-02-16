a8eae8e20b9ed1da406c4367fcb60913df2d45d7
Amin Messaoudi <amin_messaoudi@yahoo.com>
2015-02-16 17:41:57 +0100

    - Pose d'un pion - Etat::isFinished implémentée

--- src/Jeu/Etat.cpp
+++ src/Jeu/Etat.cpp
@@ -7,3 +7,24 @@
 
 #include "Etat.h"
 
+#include "Map/Map.h"
+
+
+bool Etat::isFinished()
+{
+	Map &map = Map::Instance();
+
+	for (int i = 1; i<Map::LIGNE-1; i++)
+	{
+		for (int j = 1; i<Map::COLONNE-1; i++)
+		{
+			pair<int, int> index(i,j);
+			if(!map[index].isEstOccupe())
+			{
+				return false;
+			}
+		}
+	}
+
+	return true;
+}
